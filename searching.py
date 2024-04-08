90862a726f5c1385f060901c08e7e06fd086f737
Martin Havlat <martas.havlat@seznam.cz>
2024-04-08 09:17:36 +0200

    1

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -13,9 +13,21 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open("sequential.json","r") as f:
+        allowed_key = json.load(f)
+
+    if field not in allowed_key:
+        return None
+
+    with open(file_name,"r") as f:
+        data = json.load(f)
+
+    return data.get(field)
 
 def main():
-    pass
+    #pass
+    sequential_data = read_data("sequential.json","unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
